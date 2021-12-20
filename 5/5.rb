57aed3c3610654c87d133ac736167fa74911fe8d
Taylor A Coon <Taylor.allan.coon@gmail.com>
2021-12-20 01:49:52 -0800

    clean the code up to nicely print the two different parts

--- 5/5.rb
+++ 5/5.rb
@@ -6,6 +6,8 @@ list = file_data.split("\n")
 
 matrix = Matrix.new(1000, 1000)
 
+# PART 1
+
 list.each do |line|
     first = line.split(" -> ")[0].split(',')
     second = line.split(" -> ")[1].split(',')
@@ -13,6 +15,18 @@ list.each do |line|
     first = Point.new(first[0].to_i, first[1].to_i)
     second = Point.new(second[0].to_i, second[1].to_i)
     matrix.mark_horizontal_line(first, second)
+end
+
+p matrix.count_2s
+
+# PART 2
+
+list.each do |line|
+    first = line.split(" -> ")[0].split(',')
+    second = line.split(" -> ")[1].split(',')
+
+    first = Point.new(first[0].to_i, first[1].to_i)
+    second = Point.new(second[0].to_i, second[1].to_i)
     matrix.mark_vertical_line(first, second)
 end
 
