5e715fe6849956d92c595d03cd1735e45e0b5d5a
Taylor A Coon <Taylor.allan.coon@gmail.com>
2022-11-30 23:02:08 -0800

    2022 day 1

--- 2019/2/2.rb
+++ 2019/2/2.rb
@@ -2,3 +2,45 @@ require_relative "../../Structs/Matrix"
 require_relative "../../Structs/List"
 require_relative "../../Structs/Queue"
 require_relative "../../Structs/Stack"
+
+file = File.open("input.txt")
+file_data = file.read
+
+test_file = File.open("test.txt")
+test_data = test_file.read
+
+@int_code = file_data.split(",").map(&:to_i)
+
+@int_code[1] = 12
+@int_code[2] = 2
+
+
+one = -> (pos1, pos2, pos3) {
+    @int_code[pos3] = @int_code[pos1] + @int_code[pos2]
+}
+
+two = -> (pos1, pos2, pos3) {
+    @int_code[pos3] = @int_code[pos1] * @int_code[pos2]
+}
+
+options = {
+    1 => one,
+    2 => two,
+}
+
+i = 0
+
+while i < @int_code.length - 1
+
+    code = @int_code[i]
+    pos1 = @int_code[i+1]
+    pos2 = @int_code[i+2]
+    pos3 = @int_code[i+3]
+
+    break if code == 99
+    options[code].call(pos1, pos2, pos3)
+
+    i += 4
+end
+
+p @int_code[0]
--- 2022/1/1.rb
+++ 2022/1/1.rb
@@ -2,3 +2,42 @@ require_relative "../../Structs/Matrix"
 require_relative "../../Structs/List"
 require_relative "../../Structs/Queue"
 require_relative "../../Structs/Stack"
+
+file = File.open("input.txt")
+file_data = file.read
+
+elves_bags = file_data.split("\n").map do |item|
+    if item === ""
+        item
+    else
+        item.to_i
+    end
+end
+
+total_calories_by_elf = {}
+
+elf_id = 0
+
+elves_bags.each do |food_item|
+    if food_item === ""
+        elf_id += 1
+        next
+    end
+
+    if total_calories_by_elf[elf_id]
+        total_calories_by_elf[elf_id] += food_item
+    else
+        total_calories_by_elf[elf_id] = food_item
+    end
+end
+
+sorted_values = total_calories_by_elf.values.sort
+
+first, second, third = sorted_values[-1], sorted_values[-2], sorted_values[-3]
+
+# Part 1
+p first
+
+# Part 2
+p first + second + third
+
