e61034441d9aa38e5f340e8477cdc7107cbf1d9f
Taylor Coon <tactac@amazon.com>
2022-12-06 15:59:15 -0800

    2022 day 3 part 1

--- 2022/3/3.rb
+++ 2022/3/3.rb
@@ -1,4 +1,33 @@
-require_relative "../../Structs/Matrix"
-require_relative "../../Structs/List"
-require_relative "../../Structs/Queue"
-require_relative "../../Structs/Stack"
+input_data = File.open("input.txt").read.split("\n")
+
+# input_data = File.open("test.txt").read.split("\n")
+
+
+letter_list = ("a".."z").to_a
+letter_list.concat(("A".."Z").to_a)
+
+priorities = {}
+
+letter_list.each_with_index do |e,i|
+    priorities[e] = i + 1
+end
+
+sum_of_priorities = 0
+
+input_data.each do |rucksack|
+    rucksack = rucksack.split("")
+    left, right = rucksack.each_slice( (rucksack.size/2.0).round ).to_a
+    right = right.join("")
+
+    left.each do |letter|
+        if(right.match(/#{letter}/))
+            sum_of_priorities += priorities[letter]
+            break
+        end
+    end
+end
+
+# Part 1
+p sum_of_priorities
+
+
