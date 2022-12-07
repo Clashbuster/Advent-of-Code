0d05301df1ae5369c65ecd937673bc9244351264
Taylor Coon <tactac@amazon.com>
2022-12-06 17:09:09 -0800

    2022 day4 part 1 and 2

--- 2022/4/4.rb
+++ 2022/4/4.rb
@@ -1,4 +1,33 @@
-require_relative "../../Structs/Matrix"
-require_relative "../../Structs/List"
-require_relative "../../Structs/Queue"
-require_relative "../../Structs/Stack"
+
+input_data = File.open("input.txt").read.split("\n")
+
+# input_data = File.open("test.txt").read.split("\n")
+
+
+tally_of_complete_covers = 0
+tally_of_any_overlap = 0
+
+
+input_data.each do |element|
+    sections = element.split(/[,,-]+/)
+    first_section = sections[0].to_i..sections[1].to_i
+    second_section = sections[2].to_i..sections[3].to_i
+    if(first_section.cover?(second_section) or second_section.cover?(first_section))
+        tally_of_complete_covers += 1
+    end
+
+    if(
+        first_section.cover?(sections[2].to_i) or
+        first_section.cover?(sections[3].to_i) or
+        second_section.cover?(sections[0].to_i) or
+        second_section.cover?(sections[1].to_i)
+    )
+        tally_of_any_overlap += 1
+    end
+end
+
+# Part 1
+p tally_of_complete_covers
+
+# Part 2
+p tally_of_any_overlap
