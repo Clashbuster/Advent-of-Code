105811935967046892c6bf29dc8afac919837cb1
Taylor Coon <tactac@amazon.com>
2022-12-07 09:21:17 -0800

    cleanup day 2022 day 5

--- 2022/5/5.rb
+++ 2022/5/5.rb
@@ -2,37 +2,28 @@ require_relative "../../Structs/Stack"
 
 input_data = File.open("input.txt").read.split("\n")
 
-stacks = []
+def make_stacks_array
+    stacks = []
 
-9.times do
-    stacks.push(Stack.new())
+    9.times do
+        stacks.push(Stack.new())
+    end
+
+    stacks[0].fill(["T", "R", "G", "W", "Q", "M", "F", "P"].reverse)
+    stacks[1].fill(["R", "F", "H"].reverse)
+    stacks[2].fill(["D", "S", "H", "G", "V", "R", "Z", "P"].reverse)
+    stacks[3].fill(["G", "W", "F", "B", "P", "H", "Q"].reverse)
+    stacks[4].fill(["P", "S", "M", "J", "H"])
+    stacks[5].fill(["M", "Z", "T", "H", "S", "R", "P", "L"])
+    stacks[6].fill(["P", "T", "H", "N", "M", "L"])
+    stacks[7].fill(["F", "D", "Q", "R"])
+    stacks[8].fill(["D", "S", "C", "N", "L", "P", "H"])
+
+    return stacks
 end
 
-stacks[0].fill(["T", "R", "G", "W", "Q", "M", "F", "P"].reverse)
-stacks[1].fill(["R", "F", "H"].reverse)
-stacks[2].fill(["D", "S", "H", "G", "V", "R", "Z", "P"].reverse)
-stacks[3].fill(["G", "W", "F", "B", "P", "H", "Q"].reverse)
-stacks[4].fill(["P", "S", "M", "J", "H"])
-stacks[5].fill(["M", "Z", "T", "H", "S", "R", "P", "L"])
-stacks[6].fill(["P", "T", "H", "N", "M", "L"])
-stacks[7].fill(["F", "D", "Q", "R"])
-stacks[8].fill(["D", "S", "C", "N", "L", "P", "H"])
-
-stacks1 = []
-
-9.times do
-    stacks1.push(Stack.new())
-end
-
-stacks1[0].fill(["T", "R", "G", "W", "Q", "M", "F", "P"].reverse)
-stacks1[1].fill(["R", "F", "H"].reverse)
-stacks1[2].fill(["D", "S", "H", "G", "V", "R", "Z", "P"].reverse)
-stacks1[3].fill(["G", "W", "F", "B", "P", "H", "Q"].reverse)
-stacks1[4].fill(["P", "S", "M", "J", "H"])
-stacks1[5].fill(["M", "Z", "T", "H", "S", "R", "P", "L"])
-stacks1[6].fill(["P", "T", "H", "N", "M", "L"])
-stacks1[7].fill(["F", "D", "Q", "R"])
-stacks1[8].fill(["D", "S", "C", "N", "L", "P", "H"])
+stacks = make_stacks_array()
+stacks1 = make_stacks_array()
 
 
 input_data.each do |instruction|
@@ -73,6 +64,8 @@ stacks1.each do |stack1|
     answer1.push(stack1.pop())
 end
 
+# Part 1
 p answer.join("")
 
+# Part 2
 p answer1.join("")
